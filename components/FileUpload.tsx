dcf55a1f5812f124660daeb3125d5d0050006750
Akshat Singh Thakur <developerakshatsingh@gmail.com>
2025-08-04 08:40:36 -0700

    uploadthing typerror fix

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -1,12 +1,14 @@
 "use client"
 
-import { OurFileRouter } from "@/app/api/uploadthing/core"
-import { UploadButton, UploadDropzone } from "@/lib/uploadthings"
+import { OurFileRouter } from "@/app/api/uploadthing/core";
 
+import { UploadButton } from "@/lib/uploadthings";
+
+import toast from "react-hot-toast";
 
 interface FileUploadProps {
-    endpoint: string
-    onSubmit: ({ url, name }: { url: string, name: string }) => void
+    endpoint: keyof OurFileRouter;
+    onSubmit: ({ url, name }: { url: string, name: string }) => void;
 }
 
 const FileUpload = (
@@ -14,35 +16,75 @@ const FileUpload = (
 ) => {
     return (
         <>
-            <div className="w-full h-auto flex items-center justify-center" >
-                <UploadButton<OurFileRouter, "imageUploader">
+            <div className="w-full h-auto flex items-center justify-center">
+                <UploadButton
                     className="text-black bg-blue-500 rounded-md w-32 h-17 py-4"
                     endpoint={endpoint}
-                    onClientUploadComplete={(res: any) => {
+                    onClientUploadComplete={(res) => {
                         console.log("Files: ", res);
                         if (res && res.length > 0) {
-                            onSubmit({ url: res[0].url, name: res[0].name })
+                            onSubmit({ url: res[0].url, name: res[0].name });
                         }
                     }}
                     onUploadError={(error: Error) => {
-                        alert(`ERROR! ${error.message}`);
+                        toast.error(`ERROR! ${error.message}`);
                     }}
                 />
             </div>
-            {/* <UploadDropzone<OurFileRouter, "imageUploader">
-                endpoint={endpoint}
-                onClientUploadComplete={(res: any) => {
-                    console.log("Files: ", res);
-                    if (res && res.length > 0) {
-                        onSubmit({ url: res[0].url, name: res[0].name })
-                    }
-                }}
-                onUploadError={(error: Error) => {
-                    alert(`ERROR! ${error.message}`);
-                }}
-            /> */}
         </>
-    )
+    );
 }
 
-export default FileUpload
+export default FileUpload;
+
+
+
+
+// "use client"
+
+// import { OurFileRouter } from "@/app/api/uploadthing/core"
+// import { UploadButton, UploadDropzone } from "@/lib/uploadthings"
+
+
+// interface FileUploadProps {
+//     endpoint: string
+//     onSubmit: ({ url, name }: { url: string, name: string }) => void
+// }
+
+// const FileUpload = (
+//     { endpoint, onSubmit }: FileUploadProps
+// ) => {
+//     return (
+//         <>
+//             <div className="w-full h-auto flex items-center justify-center" >
+//                 <UploadButton<OurFileRouter, "imageUploader">
+//                     className="text-black bg-blue-500 rounded-md w-32 h-17 py-4"
+//                     endpoint={endpoint}
+//                     onClientUploadComplete={(res: any) => {
+//                         console.log("Files: ", res);
+//                         if (res && res.length > 0) {
+//                             onSubmit({ url: res[0].url, name: res[0].name })
+//                         }
+//                     }}
+//                     onUploadError={(error: Error) => {
+//                         alert(`ERROR! ${error.message}`);
+//                     }}
+//                 />
+//             </div>
+//             {/* <UploadDropzone<OurFileRouter, "imageUploader">
+//                 endpoint={endpoint}
+//                 onClientUploadComplete={(res: any) => {
+//                     console.log("Files: ", res);
+//                     if (res && res.length > 0) {
+//                         onSubmit({ url: res[0].url, name: res[0].name })
+//                     }
+//                 }}
+//                 onUploadError={(error: Error) => {
+//                     alert(`ERROR! ${error.message}`);
+//                 }}
+//             /> */}
+//         </>
+//     )
+// }
+
+// export default FileUpload
